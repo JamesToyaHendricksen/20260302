d0071c0ce9e4d18f2e22443d89973b4b02178b32
LAPTOP-AMQI5BOG\academia <202511_aw_toyoda@x-ide.sakura.ne.jp>
2026-03-02 11:56:44 +0900

    MyBatisのXML方式でTodo用のMapperを作成

---
+++ src/main/java/com/example/demo/mapper/TodoMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.Todo;
+
+import java.util.List;
+
+public interface TodoMapper {
+
+    List<Todo> findAll();
+
+    int insert(Todo todo);
+}
